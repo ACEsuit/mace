9eda44fa4d80964e57b1525df1f3260909d65905
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2022-02-10 10:26:11 +0100

    Update run_train.py

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -141,10 +141,10 @@ def main() -> None:
         loss_fn = modules.EnergyForcesLoss(energy_weight=args.energy_weight, forces_weight=args.forces_weight)
     logging.info(loss_fn)
     
-    if args.compute_avg_num_neighbors:
-        args.avg_num_neighbors = modules.compute_avg_num_neighbors(train_loader)
+    if args.compute_num_avg_neighbors:
+        args.avg_num_neighbors = modules.compute_num_avg_neighbors(train_loader)
     logging.info(f'Average number of neighbors: {args.avg_num_neighbors:.3f}')
-    
+
     # Build model
     logging.info('Building model')
     model_config = dict(
