fix avg num computation

LieACE/modules/utils.py
@@ -147,18 +147,13 @@ def compute_mean_rms_energy_forces(
     return mean, rms
 
 
-def compute_num_avg_neighbors(
-    data_loader: torch.utils.data.DataLoader,
-) -> Tuple[float, float]:
-
-    average_neighbors_list = []
+def compute_num_avg_neighbors(data_loader: torch.utils.data.DataLoader) -> float:
+    num_neighbors = []
 
     for batch in data_loader:
-        _,count = torch.unique(batch.edge_index)
-        average_neighbors_list.append(torch.mean(count/2))
+        senders, receivers = batch.edge_index
+        _, counts = torch.unique(receivers, return_counts=True)
+        num_neighbors.append(counts)
 
-    average_neighbors = torch.cat(average_neighbors_list, dim=0)  # [total_n_graphs]
-    
-    num_avg_neighbors = to_numpy(torch.mean(average_neighbors)).item()
-
-    return num_avg_neighbors
+    avg_num_neighbors = torch.mean(torch.cat(num_neighbors, dim=0).type(torch.get_default_dtype()))
+    return to_numpy(avg_num_neighbors).item()