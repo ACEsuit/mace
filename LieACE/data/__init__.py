ec3612dd191bec9697d5b71b1d42514ec1123778
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2022-02-10 10:15:52 +0100

    fix imports

--- LieACE/data/__init__.py
+++ LieACE/data/__init__.py
@@ -5,7 +5,7 @@ from .neighborhood import get_neighborhood
 from .utils import Configurations
 from .three_bpa import atomic_energies as three_bpa_atomic_energies
 from .three_bpa import load as load_3bpa
-
+from .utils import Configuration, Configurations, random_train_valid_split
 
 __all__ = [
     'load_rmd17', 'rmd17_atomic_energies', 'AtomicData', 'get_neighborhood', 'Configuration', 'Configurations',
