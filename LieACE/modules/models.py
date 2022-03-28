fixed normalization

LieACE/modules/models.py
@@ -284,9 +284,7 @@ class NonLinearBodyOrderedModel(torch.nn.Module):
         vectors, lengths = get_edge_vectors_and_lengths(
             positions=data.positions, edge_index=data.edge_index, shifts=data.shifts
         )
-        edge_attrs = self.spherical_harmonics(vectors) * torch.sqrt(
-            4 * torch.tensor(np.pi, dtype=self.dtype)
-        )
+        edge_attrs = self.spherical_harmonics(vectors)
         edge_feats = self.radial_embedding(lengths)
 
         # Interactions