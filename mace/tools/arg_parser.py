fa042182012368731af4c335c5583b10e0c195d5
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2022-07-25 17:47:20 +0200

    added right keys to dataset

--- mace/tools/arg_parser.py
+++ mace/tools/arg_parser.py
@@ -183,7 +183,18 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
         type=str,
         default="forces",
     )
-
+    parser.add_argument(
+        "--virials_key",
+        help="Key of reference virials in training xyz",
+        type=str,
+        default="virials",
+    )
+    parser.add_argument(
+        "--stress_key",
+        help="Key of reference stress in training xyz",
+        type=str,
+        default="stress",
+    )
     # Loss and optimization
     parser.add_argument(
         "--loss",
--- scripts/run_train.py
+++ scripts/run_train.py
@@ -32,6 +32,8 @@ def get_dataset_from_xyz(
     seed: int = 1234,
     energy_key: str = "energy",
     forces_key: str = "forces",
+    stress_key: str = "stress",
+    virials_key: str = "virials",
 ) -> Tuple[SubsetCollection, Optional[Dict[int, float]]]:
     """Load training and test dataset from xyz file"""
     atomic_energies_dict, all_train_configs = data.load_from_xyz(
@@ -39,6 +41,8 @@ def get_dataset_from_xyz(
         config_type_weights=config_type_weights,
         energy_key=energy_key,
         forces_key=forces_key,
+        stress_key=stress_key,
+        virials_key=virials_key,
         extract_atomic_energies=True,
     )
     logging.info(
@@ -50,6 +54,8 @@ def get_dataset_from_xyz(
             config_type_weights=config_type_weights,
             energy_key=energy_key,
             forces_key=forces_key,
+            stress_key=stress_key,
+            virials_key=virials_key,
             extract_atomic_energies=False,
         )
         logging.info(
@@ -71,6 +77,8 @@ def get_dataset_from_xyz(
             config_type_weights=config_type_weights,
             energy_key=energy_key,
             forces_key=forces_key,
+            stress_key=stress_key,
+            virials_key=virials_key,
             extract_atomic_energies=False,
         )
         # create list of tuples (config_type, list(Atoms))
@@ -117,6 +125,8 @@ def main() -> None:
         seed=args.seed,
         energy_key=args.energy_key,
         forces_key=args.forces_key,
+        stress_key=args.stress_key,
+        virials_key=args.virials_key,
     )
 
     logging.info(
