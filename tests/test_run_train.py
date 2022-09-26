4b19bd3875cfe31509aaa5ff31133cb62db60a70
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2022-09-26 13:41:45 -0400

    Test run_train.py with some missing data

--- tests/test_run_train.py
+++ tests/test_run_train.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 
+import pytest
+
 from pathlib import Path
 pytest_mace_dir = Path(__file__).parent.parent
 run_train = Path(__file__).parent.parent / "scripts" / "run_train.py"
@@ -13,7 +15,8 @@ from ase.atoms import Atoms
 
 from mace.calculators.mace import MACECalculator
 
-def test_run_train(tmp_path):
+@pytest.fixture
+def fitting_configs():
     water = Atoms(numbers=[8, 1, 1], positions=[[0, -2.0, 0], [1, 0, 0], [0, 1, 0]], cell=[4]*3, pbc = [True]*3)
     fit_configs = [Atoms(numbers=[8], positions=[[0, 0, 0]], cell=[6]*3),
                Atoms(numbers=[1], positions=[[0, 0, 0]], cell=[6]*3)]
@@ -30,35 +33,67 @@ def test_run_train(tmp_path):
         c.new_array("REF_forces", np.random.normal(0.1, size=c.positions.shape))
         c.info["REF_stress"] = np.random.normal(0.1, size=6)
         fit_configs.append(c)
-    ase.io.write(tmp_path / "fit.xyz", fit_configs)
 
-    mace_params = {
-        "name": "MACE",
-        "checkpoints_dir": str(tmp_path),
-        "model_dir": str(tmp_path),
-        "train_file": tmp_path / "fit.xyz",
-        "valid_fraction": 0.05,
-        "energy_weight": 1.0,
-        "forces_weight": 10.0,
-        "stress_weight": 1.0,
-        "model": "MACE",
-        "hidden_irreps": "128x0e",
-        "r_max": 3.5,
-        "batch_size": 5,
-        "max_num_epochs": 10,
-        "swa": None,
-        "start_swa": 5,
-        "ema": None,
-        "ema_decay": 0.99,
-        "amsgrad": None,
-        "restart_latest": None,
-        "device": "cpu",
-        "seed": 5,
-        "loss": "stress",
-        "energy_key": "REF_energy",
-        "forces_key": "REF_forces",
-        "stress_key": "REF_stress"
-    }
+    return fit_configs
+
+_mace_params = {
+    "name": "MACE",
+    "valid_fraction": 0.05,
+    "energy_weight": 1.0,
+    "forces_weight": 10.0,
+    "stress_weight": 1.0,
+    "model": "MACE",
+    "hidden_irreps": "128x0e",
+    "r_max": 3.5,
+    "batch_size": 5,
+    "max_num_epochs": 10,
+    "swa": None,
+    "start_swa": 5,
+    "ema": None,
+    "ema_decay": 0.99,
+    "amsgrad": None,
+    "restart_latest": None,
+    "device": "cpu",
+    "seed": 5,
+    "loss": "stress",
+    "energy_key": "REF_energy",
+    "forces_key": "REF_forces",
+    "stress_key": "REF_stress"
+}
+
+
+def test_run_train(tmp_path, fitting_configs):
+    ase.io.write(tmp_path / "fit.xyz", fitting_configs)
+
+    mace_params = _mace_params.copy()
+    mace_params["checkpoints_dir"] = str(tmp_path)
+    mace_params["model_dir"] = str(tmp_path)
+    mace_params["train_file"] = tmp_path / "fit.xyz"
+
+    # make sure run_train.py is using the mace that is currently being tested
+    run_env = os.environ
+    run_env["PYTHONPATH"] = str(pytest_mace_dir) + ":" + os.environ["PYTHONPATH"]
+
+    cmd = sys.executable + " " + str(run_train) + " " + " ".join([(f"--{k}={v}" if v is not None else f"--{k}") for k, v in mace_params.items()])
+
+    p = subprocess.run(cmd.split(), env=run_env)
+
+    assert p.returncode == 0
+
+    calc = MACECalculator(tmp_path / "MACE.model", device="cpu")
+
+
+def test_run_train_missing_data(tmp_path, fitting_configs):
+    del fitting_configs[5].info["REF_energy"]
+    del fitting_configs[6].arrays["REF_forces"]
+    del fitting_configs[7].info["REF_stress"]
+
+    ase.io.write(tmp_path / "fit.xyz", fitting_configs)
+
+    mace_params = _mace_params.copy()
+    mace_params["checkpoints_dir"] = str(tmp_path)
+    mace_params["model_dir"] = str(tmp_path)
+    mace_params["train_file"] = tmp_path / "fit.xyz"
 
     # make sure run_train.py is using the mace that is currently being tested
     run_env = os.environ
