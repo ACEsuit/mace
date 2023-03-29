e41d462362388bdaa0c86ad8ae45cde29cbf0f36
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2023-03-29 12:50:09 +0100

    re add committee

--- mace/calculators/__init__.py
+++ mace/calculators/__init__.py
@@ -1,9 +1,15 @@
 from .lammps_mace import LAMMPS_MACE
-from .mace import DipoleMACECalculator, EnergyDipoleMACECalculator, MACECalculator
+from .mace import (
+    DipoleMACECalculator,
+    EnergyDipoleMACECalculator,
+    MACECalculator,
+    MACECommitteeCalculator,
+)
 
 __all__ = [
     "MACECalculator",
     "DipoleMACECalculator",
     "EnergyDipoleMACECalculator",
     "LAMMPS_MACE",
+    "MACECommitteeCalculator",
 ]
