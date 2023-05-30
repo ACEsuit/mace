f17963f7bb7b197e68c2a8ab83e341e4685dfbe4
Chuck Witt <witt.chuck@gmail.com>
2023-05-30 16:01:53 +0100

    add script for creating lammps model.

---
+++ scripts/create_lammps_model.py
@@ -0,0 +1,11 @@
+from e3nn.util import jit
+import sys
+import torch
+from mace.calculators import LAMMPS_MACE
+
+model_path = sys.argv[1]  # takes model name as command-line input
+model = torch.load(model_path)
+model = model.double().to("cpu")
+lammps_model = LAMMPS_MACE(model)
+lammps_model_compiled = jit.compile(lammps_model)
+lammps_model_compiled.save(model_path+"-lammps.pt")
