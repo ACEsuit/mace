1d67e69de3a3ce45d5a031661fd883853da607ab
mavaylon1 <mavaylon1@berkeley.edu>
2023-07-20 09:16:14 -0700

    clean

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -25,9 +25,8 @@ from mace.data.utils import (
 )
 from mace.tools.scripts_utils import get_dataset_from_xyz, get_atomic_energies
 from mace.tools import torch_geometric
-from mace.modules import compute_avg_num_neighbors, scaling_classes
+from mace.modules import compute_avg_num_neighbors
 from mace.modules import compute_statistics
-
 
 import concurrent.futures
 import multiprocessing as mp
@@ -39,10 +38,11 @@ results = []
 def response(result):
     results.append(result)
 
+    
 def target(receivers):
-    # _, receivers = batch.edge_index
     _, counts = torch.unique(receivers, return_counts=True)
     return counts
+
 
 def neighbor_multi_process(data_loader):
     pool = mp.Pool(processes=len(data_loader))
@@ -59,13 +59,13 @@ def neighbor_multi_process(data_loader):
     return to_numpy(avg_num_neighbors).item()
 
 
-
 compute_stats_results = []
 
 def compute_stats_callback(result):
     compute_stats_results.append(result)
 
-def compute_stats_target(file, z_table, scaling, r_max, atomic_energies, batch_size):
+    
+def compute_stats_target(file, z_table, r_max, atomic_energies, batch_size):
     train_dataset = data.HDF5Dataset(file, z_table=z_table, r_max=r_max)
     train_loader = torch_geometric.dataloader.DataLoader(
         dataset=train_dataset, 
@@ -74,36 +74,22 @@ def compute_stats_target(file, z_table, scaling, r_max, atomic_energies, batch_s
         drop_last=False,
     )
     
-    # avg_num_neighbors, mean, std = compute_statistics(train_loader, scaling, atomic_energies)
     avg_num_neighbors, mean, std = compute_statistics(train_loader, atomic_energies)
 
     output = [avg_num_neighbors, mean, std]
     return output
 
-# def pool_compute_stats(path_to_files):
-def pool_compute_stats(inputs): #inputs = (path_to_files, z_table, scaling, r_max, atomic_energies, batch_size)
-    path_to_files, z_table, scaling, r_max, atomic_energies, batch_size = inputs
+
+def pool_compute_stats(inputs): #inputs = (path_to_files, z_table, r_max, atomic_energies, batch_size)
+    path_to_files, z_table, r_max, atomic_energies, batch_size = inputs
     pool = mp.Pool(processes=int(os.cpu_count()/4))
     
-    # loaders = pool.map(compute_stats_target, glob(path_to_files+'/*'))
-    # for file in glob(path_to_files+'/*'):
-    #     res=pool.apply_async(compute_stats_target, args=(file, z_table, scaling, r_max, atomic_energies, batch_size,), callback=compute_stats_callback)
-    re=[pool.apply_async(compute_stats_target, args=(file, z_table, scaling, r_max, atomic_energies, batch_size,)) for file in glob(path_to_files+'/*')]
+    re=[pool.apply_async(compute_stats_target, args=(file, z_table, r_max, atomic_energies, batch_size,)) for file in glob(path_to_files+'/*')]
     
     pool.close()
     pool.join()
     results = [r.get() for r in tqdm.tqdm(re)]
     return np.average(results, axis=0)
-    
-# def compute_statistics(train_loader: torch.utils.data.DataLoader, 
-#                        scaling: str, 
-#                        atomic_energies: np.ndarray):
-#     """
-#     Compute the average number of neighbors and the mean energy and standard
-#     deviation of the force components"""
-#     avg_num_neighbors = compute_avg_num_neighbors(train_loader)
-#     mean, std = scaling_classes[scaling](train_loader, atomic_energies)
-#     return avg_num_neighbors, mean, std
 
 
 def split_array(a: np.ndarray, max_size: int):
@@ -193,13 +179,6 @@ def main():
     logging.info("Preparing training set")
     if args.shuffle:
         random.shuffle(collections.train)
-        
-    # with h5py.File(args.h5_prefix + "train.h5", "w") as f:
-    #     # split collections.train into batches and save them to hdf5
-    #     split_train, drop_last = split_array(collections.train, args.batch_size)
-    #     f.attrs["drop_last"] = drop_last
-    #     for i, batch in enumerate(tqdm.tqdm(split_train)):
-    #         save_configurations_as_HDF5(batch, i, f)
 
     # split collections.train into batches and save them to hdf5
     split_train = np.array_split(collections.train,int(os.cpu_count()/4))
@@ -229,35 +208,8 @@ def main():
         [atomic_energies_dict[z] for z in z_table.zs]
     )
     logging.info(f"Atomic energies: {atomic_energies.tolist()}")
-    _inputs = ['/pscratch/sd/m/mavaylon/processed_chem', z_table, args.scaling, args.r_max, atomic_energies, args.batch_size]
+    _inputs = ['/pscratch/sd/m/mavaylon/processed_chem', z_table, args.r_max, atomic_energies, args.batch_size]
     avg_num_neighbors, mean, std=pool_compute_stats(_inputs)
-    # breakpoint()
-        
-        
-        
-        # Compute statistics
-#         logging.info("Computing statistics")
-#         if len(atomic_energies_dict) == 0:
-#             atomic_energies_dict = get_atomic_energies(args.E0s, collections.train, z_table)
-#         atomic_energies: np.ndarray = np.array(
-#             [atomic_energies_dict[z] for z in z_table.zs]
-#         )
-#         logging.info(f"Atomic energies: {atomic_energies.tolist()}")
-#         train_dataset = data.HDF5Dataset(args.h5_prefix + "train.h5", z_table=z_table, r_max=args.r_max)
-#         train_loader = torch_geometric.dataloader.DataLoader(
-#             dataset=train_dataset, 
-#             batch_size=args.batch_size,
-#             num_workers=2,
-#             shuffle=False,
-#             drop_last=False,
-#         )
-        
-#         avg_num_neighbors = neighbor_multi_process(train_loader)
-        # breakpoint()
-#         # avg_num_neighbors, mean, std = compute_statistics(
-#         mean, std = compute_statistics(
-#             train_loader, args.scaling, atomic_energies
-#         )
     logging.info(f"Average number of neighbors: {avg_num_neighbors}")
     logging.info(f"Mean: {mean}")
     logging.info(f"Standard deviation: {std}")
@@ -271,41 +223,36 @@ def main():
         "atomic_numbers": str(z_table.zs),
         "r_max": args.r_max,
     }
+    
     # del train_dataset
     # del train_loader
     with open(args.h5_prefix + "statistics.json", "w") as f:
         json.dump(statistics, f)
 
     
-#     logging.info("Preparing validation set")
-#     if args.shuffle:
-#         random.shuffle(collections.valid)
-
-#     # with h5py.File(args.h5_prefix + "valid.h5", "w") as f:    
-#     #     split_valid, drop_last = split_array(collections.valid, args.batch_size)
-#     #     f.attrs["drop_last"] = drop_last
-#     #     for i, batch in enumerate(split_valid):
-#     #         save_configurations_as_HDF5(batch, i, f)
+    logging.info("Preparing validation set")
+    if args.shuffle:
+        random.shuffle(collections.valid)
     
-#     split_valid, drop_last = split_array(collections.valid, args.batch_size)
+    split_valid, drop_last = split_array(collections.valid, args.batch_size)
 
-#     def multi_valid_hdf5(process):
-#         with h5py.File(args.h5_prefix + "valid_" + str(process)+".h5", "w") as f:
-#             f.attrs["drop_last"] = drop_last
-#             save_configurations_as_HDF5(split_valid[process], process, f)
+    def multi_valid_hdf5(process):
+        with h5py.File(args.h5_prefix + "valid_" + str(process)+".h5", "w") as f:
+            f.attrs["drop_last"] = drop_last
+            save_configurations_as_HDF5(split_valid[process], process, f)
     
-#     processes = []
-#     for i in range(n_batches):
-#         p = mp.Process(target=multi_valid_hdf5, args=[i])
-#         p.start()
-#         processes.append(p)
+    processes = []
+    for i in range(int(os.cpu_count()/4)):
+        p = mp.Process(target=multi_train_hdf5, args=[i])
+        p.start()
+        processes.append(p)
         
-#     for i in processes:
-#         i.join()
+    for i in processes:
+        i.join()
 
-#     if args.test_file is not None:
-#         logging.info("Preparing test sets")
-#         for name, subset in collections.tests:
+    # if args.test_file is not None:
+    #     logging.info("Preparing test sets")
+    #     for name, subset in collections.tests:
 #             with h5py.File(args.h5_prefix + name + "_test.h5", "w") as f:
 #                 split_test, drop_last = split_array(subset, args.batch_size)
 #                 f.attrs["drop_last"] = drop_last
