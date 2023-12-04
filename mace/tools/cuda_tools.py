9305d9f31fd17bf6b7054e0e4f5a3cd3aab9a954
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2023-12-04 17:26:42 +0000

    update cuda_tools.py

--- mace/tools/cuda_tools.py
+++ mace/tools/cuda_tools.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing import Tuple
 import ase
 import torch
 from e3nn import o3
@@ -9,6 +10,10 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 from mace.modules.symmetric_contraction import SymmetricContraction
 from mace.tools import torch_geometric, torch_tools
+
+from mace_ops.ops.invariant_message_passing import InvariantMessagePassingTP
+from mace_ops.ops.linear import Linear
+from mace_ops.ops.symmetric_contraction import SymmetricContraction as CUDAContraction
 
 
 def parser():
@@ -54,6 +59,7 @@ def optimize_cuda_mace(model: MACE) -> None:
     """
     for param in model.parameters():
         param.requires_grad = False
+    dtype = get_model_dtype(model)
     n_layers = int(model.num_interactions)
     sh_irreps = o3.Irreps.spherical_harmonics(3)
     spherical_harmonics = SphericalHarmonics(
@@ -65,24 +71,140 @@ def optimize_cuda_mace(model: MACE) -> None:
     model.spherical_harmonics = spherical_harmonics
     num_elements = model.node_embedding.linear.irreps_in.num_irreps
     for i in range(n_layers):
-        symmetric_contractions = SymmetricContraction(
-            irreps_in=o3.Irreps(model.products[i].symmetric_contractions.irreps_in),
-            irreps_out=o3.Irreps(model.products[i].symmetric_contractions.irreps_out),
-            correlation=3,
-            cuda_optimized=True,
-            num_elements=num_elements,
-        )
-        symmetric_contractions.contractions[0].symm_contract.__dict__["W3"] = deepcopy(
-            model.products[i].symmetric_contractions.contractions[0].weights_max.data
-        )
-        symmetric_contractions.contractions[0].symm_contract.__dict__["W2"] = deepcopy(
-            model.products[i].symmetric_contractions.contractions[0].weights[0].data
-        )
-        symmetric_contractions.contractions[0].symm_contract.__dict__["W1"] = deepcopy(
-            model.products[i].symmetric_contractions.contractions[0].weights[-1].data
+        model.interactions[i].linear_up = linear_matmul(model.interactions[i].linear_up)
+        model.interactions[i].linear = linear_to_cuda(model.interactions[i].linear)
+        model.interactions[i].tp = InvariantMessagePassingTP()
+        if "residual" in model.interactions[i].__name__:
+            model.interactions[i].forward = invariant_residual_interaction_forward
+        else:
+            model.interactions[i].forward = invariant_interaction_forward
+        model.interactions[i].linear = linear_matmul(model.interactions[i].linear)
+        symm_contract = model.products[i].symmetric_contractions
+        all_weights = {}
+        for i in range(len(symm_contract.contractions)):
+            all_weights[str(i)] = {}
+            all_weights[str(i)][3] = (
+                symm_contract.contractions[i].weights_max.detach().clone().type(dtype)
+            )
+            all_weights[str(i)][2] = (
+                symm_contract.contractions[i].weights[0].detach().clone().type(dtype)
+            )
+            all_weights[str(i)][1] = (
+                symm_contract.contractions[i].weights[1].detach().clone().type(dtype)
+            )
+        irreps_in = o3.Irreps(model.products[i].symmetric_contractions.irreps_in)
+        coupling_irreps = o3.Irreps([irrep.ir for irrep in irreps_in])
+        irreps_out = (o3.Irreps(model.products[i].symmetric_contractions.irreps_out),)
+        symmetric_contractions = CUDAContraction(
+            coupling_irreps,
+            irreps_out,
+            all_weights,
+            nthreadX=32,
+            nthreadY=4,
+            nthreadZ=1,
+            dtype=dtype,
         )
         model.products[i].symmetric_contractions = symmetric_contractions
+        model.products[i].linear = linear_matmul(model.products[i].linear)
+        model.products[i].symmetric_contractions = symmetric_contractions
     return model
+
+
+def get_model_dtype(model: torch.nn.Module) -> torch.dtype:
+    """Get the dtype of the model"""
+    mode_dtype = next(model.parameters()).dtype
+    if mode_dtype == torch.float64:
+        return "float64"
+    if mode_dtype == torch.float32:
+        return "float32"
+    raise ValueError(f"Unknown dtype {mode_dtype}")
+
+
+class linear_matmul(torch.nn.Module):
+    def __init__(self, linear_e3nn):
+        super().__init__()
+        num_channels_in = linear_e3nn.__dict__["irreps_in"].num_irreps
+        num_channels_out = linear_e3nn.__dict__["irreps_out"].num_irreps
+        self.weights = (
+            linear_e3nn.weight.data.reshape(num_channels_in, num_channels_out)
+            / num_channels_in**0.5
+        )
+
+    def forward(self, x):
+        return torch.matmul(x, self.weights)
+
+
+def linear_to_cuda(linear):
+    return Linear(
+        linear.__dict__["irreps_in"],
+        linear.__dict__["irreps_out"],
+        linear.instructions,
+        linear.weight,
+    )
+
+
+def invariant_residual_interaction_forward(
+    self,
+    node_attrs: torch.Tensor,
+    node_feats: torch.Tensor,
+    edge_attrs: torch.Tensor,
+    edge_feats: torch.Tensor,
+    edge_index: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    sender = edge_index[0]
+    receiver = edge_index[1]
+    num_nodes = node_feats.shape[0]
+    sc = self.skip_tp(node_feats, node_attrs)
+    node_feats = self.linear_up(node_feats)
+    tp_weights = self.conv_tp_weights(edge_feats)
+    first_occurences = self.tp.calculate_first_occurences(
+        receiver, num_nodes, torch.Tensor().int()
+    )
+    message = self.tp.forward(
+        node_feats,
+        edge_attrs,
+        tp_weights.view(tp_weights.shape[0], -1, node_feats.shape[-1]),
+        sender,
+        receiver,
+        first_occurences,
+    )
+    message = self.linear(message) / self.avg_num_neighbors
+    return (
+        message,
+        sc,
+    )  # [n_nodes, channels, (lmax + 1)**2]
+
+
+def invariant_interaction_forward(
+    self,
+    node_attrs: torch.Tensor,
+    node_feats: torch.Tensor,
+    edge_attrs: torch.Tensor,
+    edge_feats: torch.Tensor,
+    edge_index: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    sender = edge_index[0]
+    receiver = edge_index[1]
+    num_nodes = node_feats.shape[0]
+    node_feats = self.linear_up(node_feats)
+    tp_weights = self.conv_tp_weights(edge_feats)
+    first_occurences = self.tp.calculate_first_occurences(
+        receiver, num_nodes, torch.Tensor().int()
+    )
+    message = self.tp.forward(
+        node_feats,
+        edge_attrs,
+        tp_weights.view(tp_weights.shape[0], -1, node_feats.shape[-1]),
+        sender,
+        receiver,
+        first_occurences,
+    )
+    message = self.linear(message) / self.avg_num_neighbors
+    message = self.skip_tp(message, node_attrs)
+    return (
+        self.reshape(message),
+        None,
+    )  # [n_nodes, channels, (lmax + 1)**2]
 
 
 def benchmark(model: MACE, benchmark_file: str, name: str) -> None:
