2c35b787fd2026e0ce3ea07d9a5827aac9e6e4e0
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2023-12-05 14:55:54 +0000

    fix the cuda opt

--- mace/tools/cuda_tools.py
+++ mace/tools/cuda_tools.py
@@ -1,14 +1,14 @@
 from copy import deepcopy
 from typing import Tuple
+from functools import partial
 import ase
 import torch
 from e3nn import o3
 from mace import data, tools
 from mace.modules.blocks import SphericalHarmonics
 from mace.modules.models import MACE
-from torch.profiler import profile, record_function, ProfilerActivity
+import torch.utils.benchmark as benchmark_
 
-from mace.modules.symmetric_contraction import SymmetricContraction
 from mace.tools import torch_geometric, torch_tools
 
 from mace_ops.ops.invariant_message_passing import InvariantMessagePassingTP
@@ -16,7 +16,7 @@ from mace_ops.ops.linear import Linear
 from mace_ops.ops.symmetric_contraction import SymmetricContraction as CUDAContraction
 
 
-def parser():
+def build_parser():
     """
     Create a parser for the command line tool.
     """
@@ -35,19 +35,19 @@ def parser():
     parser.add_argument(
         "--dtype",
         type=str,
-        default="float64",
+        default="float32",
         help="Default dtype of the model.",
     )
     parser.add_argument(
         "--benchmark",
-        type="store_true",
+        action="store_true",
         help="Benchmark the optimized model.",
         default=False,
     )
     parser.add_argument(
         "--benchmark_file",
         type=str,
-        default="benchmark.xyz",
+        default="",
         help="Path to the benchmark file.",
     )
     return parser
@@ -74,27 +74,30 @@ def optimize_cuda_mace(model: MACE) -> None:
         model.interactions[i].linear_up = linear_matmul(model.interactions[i].linear_up)
         model.interactions[i].linear = linear_to_cuda(model.interactions[i].linear)
         model.interactions[i].tp = InvariantMessagePassingTP()
-        if "residual" in model.interactions[i].__name__:
-            model.interactions[i].forward = invariant_residual_interaction_forward
+        if "Residual" in model.interactions[i].__class__.__name__:
+            model.interactions[i].forward = partial(
+                invariant_residual_interaction_forward, model.interactions[i]
+            )
         else:
-            model.interactions[i].forward = invariant_interaction_forward
-        model.interactions[i].linear = linear_matmul(model.interactions[i].linear)
+            model.interactions[i].forward = partial(
+                invariant_interaction_forward, model.interactions[i]
+            )
         symm_contract = model.products[i].symmetric_contractions
         all_weights = {}
-        for i in range(len(symm_contract.contractions)):
-            all_weights[str(i)] = {}
-            all_weights[str(i)][3] = (
-                symm_contract.contractions[i].weights_max.detach().clone().type(dtype)
+        for j in range(len(symm_contract.contractions)):
+            all_weights[str(j)] = {}
+            all_weights[str(j)][3] = (
+                symm_contract.contractions[j].weights_max.detach().clone().type(dtype)
             )
-            all_weights[str(i)][2] = (
-                symm_contract.contractions[i].weights[0].detach().clone().type(dtype)
+            all_weights[str(j)][2] = (
+                symm_contract.contractions[j].weights[0].detach().clone().type(dtype)
             )
-            all_weights[str(i)][1] = (
-                symm_contract.contractions[i].weights[1].detach().clone().type(dtype)
+            all_weights[str(j)][1] = (
+                symm_contract.contractions[j].weights[1].detach().clone().type(dtype)
             )
         irreps_in = o3.Irreps(model.products[i].symmetric_contractions.irreps_in)
         coupling_irreps = o3.Irreps([irrep.ir for irrep in irreps_in])
-        irreps_out = (o3.Irreps(model.products[i].symmetric_contractions.irreps_out),)
+        irreps_out = o3.Irreps(model.products[i].symmetric_contractions.irreps_out)
         symmetric_contractions = CUDAContraction(
             coupling_irreps,
             irreps_out,
@@ -104,20 +107,27 @@ def optimize_cuda_mace(model: MACE) -> None:
             nthreadZ=1,
             dtype=dtype,
         )
-        model.products[i].symmetric_contractions = symmetric_contractions
+        model.products[i].symmetric_contractions = SymmetricContractionWrapper(
+            symmetric_contractions
+        )
         model.products[i].linear = linear_matmul(model.products[i].linear)
-        model.products[i].symmetric_contractions = symmetric_contractions
     return model
+
+
+class SymmetricContractionWrapper(torch.nn.Module):
+    def __init__(self, symmetric_contractions):
+        super().__init__()
+        self.symmetric_contractions = symmetric_contractions
+
+    def forward(self, x, y):
+        y = y.argmax(dim=-1).int()
+        return self.symmetric_contractions(x, y).squeeze()
 
 
 def get_model_dtype(model: torch.nn.Module) -> torch.dtype:
     """Get the dtype of the model"""
-    mode_dtype = next(model.parameters()).dtype
-    if mode_dtype == torch.float64:
-        return "float64"
-    if mode_dtype == torch.float32:
-        return "float32"
-    raise ValueError(f"Unknown dtype {mode_dtype}")
+    model_dtype = next(model.parameters()).dtype
+    return model_dtype
 
 
 class linear_matmul(torch.nn.Module):
@@ -151,14 +161,14 @@ def invariant_residual_interaction_forward(
     edge_feats: torch.Tensor,
     edge_index: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    sender = edge_index[0]
-    receiver = edge_index[1]
+    sender = edge_index[0].int()
+    receiver = edge_index[1].int()
     num_nodes = node_feats.shape[0]
     sc = self.skip_tp(node_feats, node_attrs)
     node_feats = self.linear_up(node_feats)
     tp_weights = self.conv_tp_weights(edge_feats)
     first_occurences = self.tp.calculate_first_occurences(
-        receiver, num_nodes, torch.Tensor().int()
+        receiver, num_nodes, torch.Tensor([]).int()
     )
     message = self.tp.forward(
         node_feats,
@@ -183,33 +193,44 @@ def invariant_interaction_forward(
     edge_feats: torch.Tensor,
     edge_index: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    sender = edge_index[0]
-    receiver = edge_index[1]
+    sender = edge_index[0].int()
+    receiver = edge_index[1].int()
     num_nodes = node_feats.shape[0]
     node_feats = self.linear_up(node_feats)
     tp_weights = self.conv_tp_weights(edge_feats)
     first_occurences = self.tp.calculate_first_occurences(
         receiver, num_nodes, torch.Tensor().int()
     )
+    tp_weights = tp_weights.view(tp_weights.shape[0], -1, node_feats.shape[-1])
     message = self.tp.forward(
         node_feats,
         edge_attrs,
-        tp_weights.view(tp_weights.shape[0], -1, node_feats.shape[-1]),
+        tp_weights,
         sender,
         receiver,
         first_occurences,
     )
     message = self.linear(message) / self.avg_num_neighbors
-    message = self.skip_tp(message, node_attrs)
+    # message = self.skip_tp(message, node_attrs)
     return (
-        self.reshape(message),
+        message,
         None,
     )  # [n_nodes, channels, (lmax + 1)**2]
 
 
 def benchmark(model: MACE, benchmark_file: str, name: str) -> None:
     # Load data and prepare input
-    atoms_list = ase.io.read(benchmark_file, format="extxyz", index=":")
+    try:
+        atoms_list = ase.io.read(benchmark_file, format="extxyz", index=":")
+    except Exception:
+        print("Could not read file {}".format(benchmark_file))
+        from ase import build
+
+        # build very large diamond structure
+        atoms = build.bulk("C", "diamond", a=3.567, cubic=True)
+        atoms_list = [atoms.repeat((10, 10, 10))]
+        print("Number of atoms", len(atoms_list[0]))
+
     configs = [data.config_from_atoms(atoms) for atoms in atoms_list]
 
     z_table = tools.AtomicNumberTable([int(z) for z in model.atomic_numbers])
@@ -226,23 +247,15 @@ def benchmark(model: MACE, benchmark_file: str, name: str) -> None:
         drop_last=False,
     )
     batch = next(iter(data_loader)).to("cuda")
-    with torch.profiler.profile(
-        schedule=torch.profiler.schedule(
-            skip_first=20, wait=5, warmup=1, active=1, repeat=2
-        ),
-        with_stack=True,
-    ) as prof:
-        with record_function("model_inference"):
-            for _ in range(20 + 5 + 1 + 1):
-                model(batch, training=False)
-                prof.step()
-    print("CUDA inference time for {}:".format(name))
-    print(
-        prof.key_averages(group_by_stack_n=1).table(
-            sort_by="cuda_time_total", row_limit=5
-        )
-    )
+    print("num edges", batch.edge_index.shape)
 
+    # Benchmark
+    t0 = benchmark_.Timer(
+        stmt="model(batch, training=False, compute_force=True)",
+        globals={"model": model, "batch": batch},
+        label=name,
+    )
+    print(t0.timeit(500))
     return None
 
 
@@ -250,14 +263,23 @@ def main(args=None):
     """
     Optimize a MACE model for CUDA inference.
     """
-    parser = parser()
+    parser = build_parser()
     args = parser.parse_args(args)
-    torch_tools.set_default_dtype(args.default_dtype)
+    torch_tools.set_default_dtype(args.dtype)
     torch_tools.init_device("cuda")
-    model = torch.load(args.model)
+    model = torch.load(args.model).to("cuda")
     model_opt = optimize_cuda_mace(model)
-    torch.save(model_opt, args.output)
+    # get current folder of the model and append the args.output
+    model_dir = "/".join(args.model.split("/")[:-1])
+    model_opt_path = model_dir + "/" + args.output
+    torch.save(model_opt, model_opt_path)
     if args.benchmark:
+        # model_opt = torch.compile(model_opt)
         benchmark(model_opt, args.benchmark_file, "opt")
+        model = torch.load(args.model).to("cuda")
         benchmark(model, args.benchmark_file, "orig")
     return None
+
+
+if __name__ == "__main__":
+    main()
