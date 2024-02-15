208769d056ce242e82c28ffee836113f9c490314
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2024-02-15 10:33:25 +0000

    Update arg_parser.py

--- mace/tools/arg_parser.py
+++ mace/tools/arg_parser.py
@@ -91,9 +91,7 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
         ],
     )
     parser.add_argument(
-        "--r_max", help="distance cutoff (in Ang)", 
-        type=float, 
-        default=5.0
+        "--r_max", help="distance cutoff (in Ang)", type=float, default=5.0
     )
     parser.add_argument(
         "--radial_type",
@@ -228,7 +226,9 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
 
     # Dataset
     parser.add_argument(
-        "--train_file", help="Training set file, format is .xyz or .h5", type=str, 
+        "--train_file",
+        help="Training set file, format is .xyz or .h5",
+        type=str,
         required=True,
     )
     parser.add_argument(
@@ -273,7 +273,7 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--pin_memory",
         help="Pin memory for data loading",
-        default=True, 
+        default=True,
         type=bool,
     )
     parser.add_argument(
@@ -581,6 +581,7 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
     )
     return parser
 
+
 def build_preprocess_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -599,9 +600,9 @@ def build_preprocess_arg_parser() -> argparse.ArgumentParser:
     )
     parser.add_argument(
         "--num_process",
-        help="The user defined number of processes to use, as well as the number of files created.", 
-        type=int, 
-        default=int(os.cpu_count()/4)
+        help="The user defined number of processes to use, as well as the number of files created.",
+        type=int,
+        default=int(os.cpu_count() / 4),
     )
     parser.add_argument(
         "--valid_fraction",
@@ -624,9 +625,7 @@ def build_preprocess_arg_parser() -> argparse.ArgumentParser:
         default="",
     )
     parser.add_argument(
-        "--r_max", help="distance cutoff (in Ang)", 
-        type=float, 
-        default=5.0
+        "--r_max", help="distance cutoff (in Ang)", type=float, default=5.0
     )
     parser.add_argument(
         "--config_type_weights",
@@ -684,9 +683,9 @@ def build_preprocess_arg_parser() -> argparse.ArgumentParser:
         default=False,
     )
     parser.add_argument(
-        "--batch_size", 
-        help="batch size to compute average number of neighbours", 
-        type=int, 
+        "--batch_size",
+        help="batch size to compute average number of neighbours",
+        type=int,
         default=16,
     )
 
