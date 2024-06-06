fix the gradient computation

mace/data/atomic_data.py
@@ -86,7 +86,7 @@ class AtomicData(torch_geometric.data.Data):
         assert virials is None or virials.shape == (1, 3, 3)
         assert dipole is None or dipole.shape[-1] == 3
         assert charges is None or charges.shape == (num_nodes,)
-        assert polarizability is None or polarizability.shape == (3, 3)
+        assert polarizability is None or polarizability.shape == (1, 3, 3)
         # Aggregate data
         data = {
             "num_nodes": num_nodes,
@@ -212,7 +212,7 @@ class AtomicData(torch_geometric.data.Data):
         )
         polarizability = (
             torch.tensor(config.polarizability, dtype=torch.get_default_dtype()).view(
-                3, 3
+                1, 3, 3
             )
             if config.polarizability is not None
             else None

mace/modules/blocks.py
@@ -84,8 +84,8 @@ class LinearDipoleReadoutBlock(torch.nn.Module):
         super().__init__()
         if dipole_only:
             self.irreps_out = o3.Irreps("1x1o")
-        elif dipole_polar:
-            self.irreps_out = o3.Irreps("1x0e + 1x1o + 1x1e")
+        if dipole_polar:
+            self.irreps_out = o3.Irreps("1x0e + 1x1o + 1x2e")
         else:
             self.irreps_out = o3.Irreps("1x0e + 1x1o")
         self.linear = o3.Linear(irreps_in=irreps_in, irreps_out=self.irreps_out)
@@ -108,8 +108,8 @@ class NonLinearDipoleReadoutBlock(torch.nn.Module):
         self.hidden_irreps = MLP_irreps
         if dipole_only:
             self.irreps_out = o3.Irreps("1x1o")
-        elif dipole_polar:
-            self.irreps_out = o3.Irreps("1x0e + 1x1o + 1x1e")
+        if dipole_polar:
+            self.irreps_out = o3.Irreps("1x0e + 1x1o + 1x2e")
         else:
             self.irreps_out = o3.Irreps("1x0e + 1x1o")
         irreps_scalars = o3.Irreps(

mace/modules/loss.py
@@ -353,7 +353,7 @@ class DipolePolarLoss(torch.nn.Module):
     def __repr__(self):
         return (
             f"{self.__class__.__name__}("
-            f"dipole_weight={self.dipole_weight:.3f}), polarizability_weight={self.polarizability_weight:.3f})"
+            f"dipole_weight={self.dipole_weight:.3f}, polarizability_weight={self.polarizability_weight:.3f})"
         )
 
 

mace/modules/models.py
@@ -798,7 +798,9 @@ class AtomicDipolesMACE(torch.nn.Module):
             )
             node_out = readout(node_feats).squeeze(-1)  # [n_nodes,3]
             if self.use_polarizability:
+                print("node_out.shape", node_out.shape)
                 node_dipoles = node_out[:, 1:4]
+                print("node_dipoles", node_dipoles.shape)
                 node_polarizability = torch.cat(
                     (node_out[:, 0].unsqueeze(-1), node_out[:, 4:]), dim=-1
                 )
@@ -842,7 +844,7 @@ class AtomicDipolesMACE(torch.nn.Module):
                 dim_size=num_graphs,
             )  # [n_graphs,6]
             total_polarizability = spherical_to_cartesian(
-                total_polarizability_spherical, device=data["positions"].device
+                total_polarizability_spherical
             )
 
             if compute_dielectric_derivatives:

mace/modules/utils.py
@@ -353,18 +353,16 @@ def compute_dielectric_gradients(
 
     def get_vjp(v):
         return torch.autograd.grad(
-            dielectric,
+            dielectric.sum(0),
             positions,
             v,
             retain_graph=True,
-            create_graph=True,
-            allow_unused=True,
+            create_graph=False,
+            allow_unused=False,
         )
 
     I_N = torch.eye(dielectric.shape[-1]).to(dielectric.device)
-    gradient = torch.vmap(get_vjp, in_dims=1, out_dims=-1)(
-        I_N.unsqueeze(0).repeat(positions.shape[0], 1, 1)
-    )[0]
+    gradient = torch.vmap(get_vjp, in_dims=1, out_dims=-1)(I_N)[0]
     if gradient[0] is None:
         return torch.zeros((positions.shape[0], dielectric.shape[-1], 3))
     return gradient

mace/tools/train.py
@@ -457,7 +457,7 @@ class MACELoss(Metric):
             )
             self.delta_polarizability_per_atom.append(
                 (batch.polarizability - output["polarizability"])
-                / (batch.ptr[1:] - batch.ptr[:-1]).unsqueeze(-1)
+                / (batch.ptr[1:] - batch.ptr[:-1]).unsqueeze(-1).unsqueeze(-1)
             )
 
     def convert(self, delta: Union[torch.Tensor, List[torch.Tensor]]) -> np.ndarray: