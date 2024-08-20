c84f0f55cdc1408c303f70003aaf56a60d2808f6
vue1999 <vue1999@gmail.com>
2024-08-20 11:58:12 +0200

    Print indices when creating valid, only warn about batch size errors

--- mace/cli/run_train.py
+++ mace/cli/run_train.py
@@ -164,14 +164,10 @@ def run(args: argparse.Namespace) -> None:
             logging.warning(
                 f"Batch size ({args.batch_size}) is larger than the number of training data ({len(collections.train)})"
             )
-            args.batch_size = int(len(collections.train) * 0.1)
-            logging.warning(f"Batch size changed to {args.batch_size}")
-        if len(collections.train) < len(collections.valid):
+        if len(collections.valid) < args.valid_batch_size:
             logging.warning(
                 f"Validation batch size ({args.valid_batch_size}) is larger than the number of validation data ({len(collections.valid)})"
             )
-            args.valid_batch_size = int(len(collections.valid) * 0.1)
-            logging.warning(f"Validation batch size changed to {args.valid_batch_size}")
 
     else:
         atomic_energies_dict = None
