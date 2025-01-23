a3ccd7496054bdbcbdaa05c7b3f7df180a7e8003
Hubert Beck <hubert.beck@matfyz.cuni.cz>
2025-01-23 09:30:28 +0100

    bug fix

--- mace/tools/model_script_utils.py
+++ mace/tools/model_script_utils.py
@@ -172,7 +172,6 @@ def _build_model(
             pair_repulsion=args.pair_repulsion,
             distance_transform=args.distance_transform,
             correlation=args.correlation,
-            gate=modules.gate_dict[args.gate],
             interaction_cls_first=modules.interaction_classes[args.interaction_first],
             MLP_irreps=o3.Irreps(args.MLP_irreps),
             atomic_inter_scale=args.std,
