df1f818aa3b1f1dfdd3cf2ea5e8e2b8c9bbc729b
Bradley A. A. Martin <bradley.aa.martin@gmail.com>
2025-04-15 13:42:47 +0000

    model_config already seems to have the additional inputs

--- mace/tools/model_script_utils.py
+++ mace/tools/model_script_utils.py
@@ -221,18 +221,7 @@ def _build_model(
         )
     if args.model == "ScaleShiftFieldMACE":
         return modules.ScaleShiftFieldMACE(
-            **model_config,
-            pair_repulsion=args.pair_repulsion,
-            distance_transform=args.distance_transform,
-            correlation=args.correlation,
-            gate=modules.gate_dict[args.gate],
-            interaction_cls_first=modules.interaction_classes[args.interaction_first],
-            MLP_irreps=o3.Irreps(args.MLP_irreps),
-            atomic_inter_scale=args.std,
-            atomic_inter_shift=args.mean,
-            radial_MLP=ast.literal_eval(args.radial_MLP),
-            radial_type=args.radial_type,
-            heads=heads,
+            **model_config
         )
     if args.model == "FoundationMACE":
         return modules.ScaleShiftMACE(**model_config_foundation)
