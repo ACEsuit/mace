b0b4a8c2fbf518f2fecfd9ddd9d030ecd2d67992
Bradley A. A. Martin <bradley.aa.martin@gmail.com>
2025-05-01 14:54:44 +0000

    added plotting scripts

--- mace/cli/plot_train.py
+++ mace/cli/plot_train.py
@@ -10,9 +10,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-fig_width = 2.5
-fig_height = 2.1
-
 plt.rcParams.update({"font.size": 6})
 
 colors = [
@@ -34,9 +31,7 @@ class RunInfo:
     name: str
     seed: int
 
-
 name_re = re.compile(r"(?P<name>.+)_run-(?P<seed>\d+)_train.txt")
-
 
 def parse_path(path: str) -> RunInfo:
     match = name_re.match(os.path.basename(path))
@@ -44,7 +39,6 @@ def parse_path(path: str) -> RunInfo:
         raise RuntimeError(f"Cannot parse {path}")
 
     return RunInfo(name=match.group("name"), seed=int(match.group("seed")))
-
 
 def parse_training_results(path: str) -> List[dict]:
     run_info = parse_path(path)
@@ -58,7 +52,6 @@ def parse_training_results(path: str) -> List[dict]:
 
     return results
 
-
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Plot mace training statistics",
@@ -70,19 +63,31 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--min_epoch", help="minimum epoch", default=0, type=int, required=False
     )
+    parser.add_argument(
+        "--compute_field", help="compute field", action="store_true", default=False, required=False
+    )
     return parser.parse_args()
 
-
-def plot(data: pd.DataFrame, min_epoch: int, output_path: str) -> None:
+def plot(data: pd.DataFrame, min_epoch: int, output_path: str, compute_field: bool) -> None:
     data = data[data["epoch"] > min_epoch]
 
-    data = data.groupby(["name", "mode", "epoch"]).agg([np.mean, np.std]).reset_index()
+    data = data.groupby(["name", "mode", "epoch"]).agg(["mean", "std"]).reset_index()
 
     valid_data = data[data["mode"] == "eval"]
     train_data = data[data["mode"] == "opt"]
 
+    fig_width = 2.5
+    fig_height = 2.5
+
+    if compute_field:
+        ncols = 3
+        fig_width *= 3
+    else:
+        ncols = 2
+        fig_width *= 2
+
     fig, axes = plt.subplots(
-        nrows=1, ncols=3, figsize=(3 * fig_width, fig_height), constrained_layout=True
+        nrows=1, ncols=ncols, figsize=(fig_width, fig_height), constrained_layout=True
     )
 
     ax = axes[0]
@@ -123,72 +128,92 @@ def plot(data: pd.DataFrame, min_epoch: int, output_path: str) -> None:
     ax.legend()
 
     ax = axes[1]
-    # ax.plot(
-    #     valid_data["epoch"],
-    #     valid_data["mae_e"]["mean"],
-    #     color=colors[1],
-    #     zorder=1,
-    #     label="MAE Energy [eV]",
-    # )
-    # ax.fill_between(
-    #     x=valid_data["epoch"],
-    #     y1=valid_data["mae_e"]["mean"] - valid_data["mae_e"]["std"],
-    #     y2=valid_data["mae_e"]["mean"] + valid_data["mae_e"]["std"],
-    #     alpha=0.5,
-    #     zorder=-1,
-    #     color=colors[1],
-    # )
-    # ax.plot(
-    #     valid_data["epoch"],
-    #     valid_data["mae_f"]["mean"],
-    #     color=colors[2],
-    #     zorder=1,
-    #     label="MAE Forces [eV/Å]",
-    # )
-    # ax.fill_between(
-    #     x=valid_data["epoch"],
-    #     y1=valid_data["mae_f"]["mean"] - valid_data["mae_f"]["std"],
-    #     y2=valid_data["mae_f"]["mean"] + valid_data["mae_f"]["std"],
-    #     alpha=0.5,
-    #     zorder=-1,
-    #     color=colors[2],
-    # )
     ax.plot(
         valid_data["epoch"],
-        valid_data["mae_bec"]["mean"],
+        valid_data["mae_e"]["mean"],
         color=colors[0],
         zorder=1,
-        label="MAE BECs [Å]",
+        label="Energy [eV]",
     )
     ax.fill_between(
         x=valid_data["epoch"],
-        y1=valid_data["mae_bec"]["mean"] - valid_data["mae_bec"]["std"],
-        y2=valid_data["mae_bec"]["mean"] + valid_data["mae_bec"]["std"],
+        y1=valid_data["mae_e"]["mean"] - valid_data["mae_e"]["std"],
+        y2=valid_data["mae_e"]["mean"] + valid_data["mae_e"]["std"],
         alpha=0.5,
         zorder=-1,
         color=colors[0],
     )
+    ax.plot(
+        valid_data["epoch"],
+        valid_data["mae_f"]["mean"],
+        color=colors[1],
+        zorder=1,
+        label="Forces [eV/Å]",
+    )
+    ax.fill_between(
+        x=valid_data["epoch"],
+        y1=valid_data["mae_f"]["mean"] - valid_data["mae_f"]["std"],
+        y2=valid_data["mae_f"]["mean"] + valid_data["mae_f"]["std"],
+        alpha=0.5,
+        zorder=-1,
+        color=colors[1],
+    )
     ax.set_xlabel("Epoch")
+    ax.set_ylabel("Valid MAE")
+    ax.set_yscale("log")
     ax.legend()
 
-    ax = axes[2]
-    ax.plot(
-        valid_data["epoch"],
-        valid_data["rmse_bec"]["mean"],
-        color=colors[3],
-        zorder=1,
-        label="RMSE BECs [Å]",
-    )
-    ax.fill_between(
-        x=valid_data["epoch"],
-        y1=valid_data["rmse_bec"]["mean"] - valid_data["rmse_bec"]["std"],
-        y2=valid_data["rmse_bec"]["mean"] + valid_data["rmse_bec"]["std"],
-        alpha=0.5,
-        zorder=-1,
-        color=colors[3],
-    )
-    ax.set_xlabel("Epoch")
-    ax.legend()
+    if compute_field:
+        ax = axes[2]
+        ax.plot(
+            valid_data["epoch"],
+            valid_data["mae_polarisation"]["mean"],
+            color=colors[0],
+            zorder=1,
+            label="Pol [eV/Å²]",
+        )
+        ax.fill_between(
+            x=valid_data["epoch"],
+            y1=valid_data["mae_polarisation"]["mean"] - valid_data["mae_polarisation"]["std"],
+            y2=valid_data["mae_polarisation"]["mean"] + valid_data["mae_polarisation"]["std"],
+            alpha=0.5,
+            zorder=-1,
+            color=colors[0],
+        )
+        ax.plot(
+            valid_data["epoch"],
+            valid_data["mae_bec"]["mean"],
+            color=colors[1],
+            zorder=1,
+            label="BECs [e]",
+        )
+        ax.fill_between(
+            x=valid_data["epoch"],
+            y1=valid_data["mae_bec"]["mean"] - valid_data["mae_bec"]["std"],
+            y2=valid_data["mae_bec"]["mean"] + valid_data["mae_bec"]["std"],
+            alpha=0.5,
+            zorder=-1,
+            color=colors[1],
+        )
+        ax.plot(
+            valid_data["epoch"],
+            valid_data["mae_polarisability"]["mean"],
+            color=colors[3],
+            zorder=1,
+            label="Alp [Å]",
+        )
+        ax.fill_between(
+            x=valid_data["epoch"],
+            y1=valid_data["mae_polarisability"]["mean"] - valid_data["mae_polarisability"]["std"],
+            y2=valid_data["mae_polarisability"]["mean"] + valid_data["mae_polarisability"]["std"],
+            alpha=0.5,
+            zorder=-1,
+            color=colors[3],
+        )
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("Valid MAE")
+        ax.set_yscale("log")
+        ax.legend()
 
     fig.savefig(output_path)
     plt.close(fig)
@@ -218,7 +243,7 @@ def run(args: argparse.Namespace) -> None:
     )
 
     for name, group in data.groupby("name"):
-        plot(group, min_epoch=args.min_epoch, output_path=f"{name}.pdf")
+        plot(group, min_epoch=args.min_epoch, output_path=f"{name}.png", compute_field=args.compute_field)
 
 
 if __name__ == "__main__":
