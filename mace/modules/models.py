844db5588c352fd75bf0c77ff4a05dee766f09d6
Bradley A. A. Martin <bradley.aa.martin@gmail.com>
2025-06-02 16:07:50 +0000

    Correct batch sizing for electric field

--- mace/modules/models.py
+++ mace/modules/models.py
@@ -619,7 +619,7 @@ class ScaleShiftFieldMACE(MACE):
             node_charge_feats, node_electronic_dipole_feats = node_field_feats[:,:,0],  node_field_feats[:,:,1:] # [n_nodes, k, 1], [n_nodes, k, 3]
             node_ionic_dipole_feats = torch.einsum('ij,ik->ijk', node_charge_feats, positions) # [n_nodes, k, 3]
             node_dipole_feats = node_ionic_dipole_feats - node_electronic_dipole_feats # [n_nodes, k, 3]
-            node_field_energies = torch.einsum('ijk,ik->ij', node_dipole_feats, electric_field.repeat_interleave(num_graphs, dim=0))
+            node_field_energies = torch.einsum('ijk,ik->ij', node_dipole_feats, electric_field.repeat_interleave(data["ptr"][1:] - data["ptr"][:-1], dim=0))
             node_feats = (node_feats.view(node_field_energies.shape[0], node_field_energies.shape[1], -1) - node_field_energies.unsqueeze(-1)).view(node_field_energies.shape[0], -1)
             node_feats_list.append(node_feats)
             node_es_list.append(
