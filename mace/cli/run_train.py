64ebd988f3cbfa536f97762da93ebfbd60f20508
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2025-08-01 11:05:06 -0400

    command line arg to set real to pretrain data ratio at which real data is duplicated

--- mace/cli/run_train.py
+++ mace/cli/run_train.py
@@ -352,16 +352,16 @@ def run(args) -> None:
         assert head_config_pt is not None, "Pretraining head not found"
         if all_ase_readable:
             ratio_pt_ft = (size_collections_train - len(head_config_pt.collections.train)) / len(head_config_pt.collections.train)
-            if ratio_pt_ft < 0.1:
+            if ratio_pt_ft < args.real_pt_data_ratio_threshold:
                 logging.warning(
                     f"Ratio of the number of configurations in the training set and the in the pt_train_file is {ratio_pt_ft}, "
-                    f"increasing the number of configurations in the fine-tuning heads by {int(0.1 / ratio_pt_ft)}"
+                    f"increasing the number of configurations in the fine-tuning heads by {int(args.real_pt_data_ratio_threshold / ratio_pt_ft)}"
                 )
                 for head_config in head_configs:
                     if head_config.head_name == "pt_head":
                         continue
                     head_config.collections.train += (
-                        head_config.collections.train * int(0.1 / ratio_pt_ft)
+                        head_config.collections.train * int(args.real_pt_data_ratio_threshold / ratio_pt_ft)
                     )
             logging.info(
                 f"Total number of configurations in pretraining: train={len(head_config_pt.collections.train)}, valid={len(head_config_pt.collections.valid)}"
