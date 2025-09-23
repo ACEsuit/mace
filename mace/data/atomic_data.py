8d5d8c8ff7bfc2c9a1958eef46bfeda987b6430d
Hatem Helal <hatem.helal@gmail.com>
2025-09-23 14:21:18 +0100

    Revert setting requires grad in data pipeline

--- mace/data/atomic_data.py
+++ mace/data/atomic_data.py
@@ -340,12 +340,10 @@ class AtomicData(torch_geometric.data.Data):
             if pbc is not None
             else torch.tensor([[False, False, False]], dtype=torch.bool)
         )
-        positions = torch.tensor(config.positions, dtype=torch.get_default_dtype())
-        positions.requires_grad_(True)
 
         return cls(
             edge_index=torch.tensor(edge_index, dtype=torch.long),
-            positions=positions,
+            positions=torch.tensor(config.positions, dtype=torch.get_default_dtype()),
             shifts=torch.tensor(shifts, dtype=torch.get_default_dtype()),
             unit_shifts=torch.tensor(unit_shifts, dtype=torch.get_default_dtype()),
             cell=cell,
