985c7fe461293d2eb56bba14a9b82c6750e51ed8
vue1999 <ev333@cam.ac.uk>
2025-11-05 01:07:27 +0000

    Revert "fix failing test on python 3.8 due to mixed dtype"
    This reverts commit 524a5f007cee4b857a94e34c16b7d6183a47203c.

--- tests/test_models.py
+++ tests/test_models.py
@@ -233,11 +233,10 @@ def test_dipole_polar_mace():
         == atomic_data.polarizability.shape
     )
     # test equivariance of output polarizability
-    pol0_rotated = np.array(
-        rot @ output["polarizability"][0].detach().numpy() @ rot.T, dtype=np.float64
+    assert np.allclose(
+        np.array(rot @ output["polarizability"][0].detach().numpy() @ rot.T),
+        output["polarizability"][1].detach().numpy(),
     )
-    pol1 = np.array(output["polarizability"][1].detach().numpy(), dtype=np.float64)
-    assert np.allclose(pol0_rotated, pol1)
 
 
 def test_energy_dipole_mace():
