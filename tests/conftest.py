382bcbed7972ebf04ec9e400bdf3ecfa0cef39dc
Tamas K Stenczel <tks32@cam.ac.uk>
2026-01-01 10:36:51 +0000

    add test duration to pytest log report and update log prefix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 import shutil
-import pytest
+
 
 def pytest_runtest_logreport(report):
-    if report.when == 'call':
+    """Prints a line about available disc space & test duration after each test."""
+    if report.when == "call":
         total, used, free = shutil.disk_usage("/")
-        print(f"\n[DISK] After {report.nodeid}: {used // (2**30)}GB used, {free // (2**30)}GB free")
+        print(
+            f"\n[METRICS] "
+            f"{report.nodeid}: "
+            f"disc: {free / (2**30):.3f}GB free, "
+            f"time: {report.duration:.2f}s"
+        )
