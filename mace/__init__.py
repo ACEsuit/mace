73eec3ddc5e3e787d970aa0a8e5a36181111dd2c
chenghjilai <chengjilai@sjtu.edu.cn>
2026-05-25 20:21:52 +0800

    tighten warnings filter scope with category and module parameters

--- mace/__init__.py
+++ mace/__init__.py
@@ -5,7 +5,10 @@ from .__version__ import __version__
 
 warnings.filterwarnings(
     "ignore",
-    message=".*TorchScript type system.*instance-level annotations.*",
+    category=UserWarning,
+    module=r"torch\.jit",
+    message=r"The TorchScript type system doesn't support instance-level annotations "
+    r"on empty non-base types in `__init__`",
 )
 
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
