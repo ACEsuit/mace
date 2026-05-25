2c86608e213489bf968d8cb2aa65b94b9a04d69b
chenghjilai <chengjilai@sjtu.edu.cn>
2026-05-25 20:07:01 +0800

    suppress TorchScript instance-level annotation warning (pytorch#89064)

--- mace/__init__.py
+++ mace/__init__.py
@@ -1,5 +1,11 @@
 import os
+import warnings
 
 from .__version__ import __version__
+
+warnings.filterwarnings(
+    "ignore",
+    message=".*TorchScript type system.*instance-level annotations.*",
+)
 
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
