8673bb8834f31ba622f2f385ed2ebb081ce30fc6
chenghjilai <chengjilai@sjtu.edu.cn>
2026-05-25 20:36:17 +0800

    Revert "tighten warnings filter scope with category and module parameters"
    This reverts commit 73eec3ddc5e3e787d970aa0a8e5a36181111dd2c.

--- mace/__init__.py
+++ mace/__init__.py
@@ -5,10 +5,7 @@ from .__version__ import __version__
 
 warnings.filterwarnings(
     "ignore",
-    category=UserWarning,
-    module=r"torch\.jit",
-    message=r"The TorchScript type system doesn't support instance-level annotations "
-    r"on empty non-base types in `__init__`",
+    message=".*TorchScript type system.*instance-level annotations.*",
 )
 
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
